101770bd57d9198c3eb05beeeec710677ca39d41
Muhammad Shiraz <muhammad.shiraz@doublelinepartners.com>
2015-02-01 23:59:22 -0600

    Switched to Mqtt example code

--- src/masSim/world/MqttMessagingProvider.java
+++ src/masSim/world/MqttMessagingProvider.java
@@ -1,6 +1,15 @@
 package masSim.world;
+import java.io.UnsupportedEncodingException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.sample.MQTTAgent;
 
 import masSim.schedule.SchedulingCommandType;
 import masSim.schedule.SchedulingEvent;
@@ -8,31 +17,36 @@ import masSim.schedule.SchedulingEventListener;
 import masSim.taems.IAgent;
 import masSim.taems.Task;
 
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.MqttTopic;
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
-import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
+//import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+//import org.eclipse.paho.client.mqttv3.MqttCallback;
+//import org.eclipse.paho.client.mqttv3.MqttClient;
+//import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+//import org.eclipse.paho.client.mqttv3.MqttException;
+//import org.eclipse.paho.client.mqttv3.MqttMessage;
+//import org.eclipse.paho.client.mqttv3.MqttTopic;
+//import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+//import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
 
-public class MqttMessagingProvider implements MqttCallback{
+public class MqttMessagingProvider extends TestMQTTAgent {
 
 	private boolean simulationMode = true;
 	private String baseTopic;
 	private int qos;
 	private String broker;
-	private MemoryPersistence persistence;
-	private MqttClient client;
+	//private MemoryPersistence persistence;
+	private MQTTAgent client;
 	private static MqttMessagingProvider provider;
 	private List<SchedulingEventListener> schedulingEventListeners = new ArrayList<SchedulingEventListener>();
 	
 	public static synchronized MqttMessagingProvider GetMqttProvider()
 	{
 		if (provider==null){
-			provider = new MqttMessagingProvider();
+			try {
+				provider = new MqttMessagingProvider();
+			} catch (MqttException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			System.out.println("Mqtt Ready");
 		}
 		return provider;
@@ -44,28 +58,46 @@ public class MqttMessagingProvider implements MqttCallback{
 			schedulingEventListeners.add(listener);
 	}
 	
-	private MqttMessagingProvider()
+	private MqttMessagingProvider() throws MqttException
 	{
 		if (!simulationMode)
-		{
-			baseTopic = "txstate/rp/masSim/";
-			qos = 2;
-			broker = "tcp://test.mosquitto.org:1883";
-			persistence = new MemoryPersistence();
 			try {
-				//client = new MqttClient("tcp://test.mosquitto.org:1883", "MasSimMqttClient", persistence);
-				//client = new MqttClient("tcp://dev.rabbitmq.com:1883", "MasSimMqttClient", persistence);
-				client = new MqttClient("tcp://localhost:1883", "MasSimMqttClient", persistence);
-				client.setCallback(this);
-				MqttConnectOptions connOpts = new MqttConnectOptions();
-	            connOpts.setCleanSession(true);
-	            client.setTimeToWait(9000);
-				client.connect(connOpts);
-	            System.out.println("Mqtt connected");
-			} catch(MqttException me) {
-	        	DisplayMqttException(me);
-	        }
-		}
+					String nodeID = getNodeMacAddress();
+					String brokerIPAddress =  "127.0.0.1"; //broken in this case is running in the same computer, it can be running in a different machine 
+					int brokerPort = 1883; //default port for Really Small Message Broker - RSMB https://www.ibm.com/developerworks/community/groups/service/html/communityview?communityUuid=d5bedadd-e46f-4c97-af89-22d65ffee070
+					String topic = "txstate/rp/masSim/"; //each MQTT agent can subscribe to multiple topics.  MacAddress + "In" is a default topic for each agent
+					//String brokerUrl, String clientId, boolean cleanSession, boolean quietMode, String userName, String password
+					String protocol = "tcp://";
+					String url = protocol + brokerIPAddress + ":" + brokerPort;
+					DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+					Calendar cal = Calendar.getInstance();
+					
+					String publishMessage = "Node: " + nodeID + " Time: " + dateFormat.format(cal.getTime());
+					
+					client = new MQTTAgent(url,nodeID,false, true, null, null);
+					client.setCallBack(this);
+					
+					//QoS (0-means FireAndForget which is fastest; 1- means StoreAndForwardWithDuplicate which is bit slow; 2- means StoreAndForwardWithoutDuplciate which is slowest
+					
+					
+//					
+//					MessageQulityOfService[] subscriptionTopicQoS = { MessageQulityOfService.StoreAndForwardWithPossibleDuplicates }; //for each subscribed topic, there is an assigned QoS level, which is intuitive.
+//					String publishTopic = nodeID + "Out"; //each MQTT agent can publish to any topics.  Default value is Mac
+//					MessageQulityOfService publishTopicQoS = MessageQulityOfService.StoreAndForwardWithPossibleDuplicates;
+//					
+//					MQTTManager mqAgent = new MQTTManager( nodeID, brokerIPAddress, brokerPort);
+//					//as a library, the MQTT manager has to be both subscriber and publisher.  Well it is a non-intuitive requirement
+					//but given the task for both the evaluation application and the runtime verification middleware, to combine both together
+					//is quickest and turns out most efficient in saving threads/avoiding concurrent common pitfalls -> especially when 
+					//mqtt is required for Android.
+//					mqAgent.setSubscribeTopics(subscriptionTopic, subscriptionTopicQoS);
+//					mqAgent.connectToBroker(); //synchronous call, only when connected, when proceed to the next step.  Asynchronous connection is built, but not exposed yet
+//					mqAgent.publishResponse(publishTopic, "Hello World".getBytes(), publishTopicQoS);
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	}
 	
 	public void SubscribeForAgent(String agentName)
@@ -73,7 +105,7 @@ public class MqttMessagingProvider implements MqttCallback{
 		if (!this.simulationMode)
 		{
 			try {
-				client.subscribe(GetAgentSpecificTopic(agentName));
+				client.subscribe(GetAgentSpecificTopic(agentName), 1);
 			} catch(MqttException me) {
 	        	DisplayMqttException(me);
 	        }
@@ -100,7 +132,7 @@ public class MqttMessagingProvider implements MqttCallback{
 		        message.setPayload(messageString.getBytes());
 		        message.setQos(qos);
 		        String topicForAgent = baseTopic + agentName;
-		        client.publish(topicForAgent, message);
+		        client.publish(topicForAgent, 1, messageString.getBytes());
 		    } catch (MqttException e) {
 		        e.printStackTrace();
 		    }
@@ -140,9 +172,13 @@ public class MqttMessagingProvider implements MqttCallback{
 	}
 
 	@Override
-	public void messageArrived(String topic, MqttMessage message)
-	        throws Exception {
-		ProcessArrivedMessage(message.toString());
+	public void messageArrived(String topic, MqttMessage message) {
+		try {
+			ProcessArrivedMessage(new String(message.getPayload(), "UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	private void ProcessArrivedMessage(String message)
